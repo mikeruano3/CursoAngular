2019e80cfc87a7b9700fb3db70887bee185ec908
Miguel Ruano <miguelruano35@gmail.com>
2020-03-17 18:59:08 -0600

    nuevo curso UDEMY

--- CursoAngular7-DirectivasNg/src/app/body/body.component.ts
+++ CursoAngular7-DirectivasNg/src/app/body/body.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent implements OnInit {
-
+  categorias: any = [ { codigo: 1, texto: 'Politica'}, { codigo: 2, texto:  'Educacion' }, { codigo: 3, texto: 'Seguridad'}];
+  noticias: any = [
+          { categoria: {
+              codigo : 1,
+              texto : 'Politica'
+            },
+            titulo : 'Los vecinos fueron los primeros en enterarse',
+            texto: 'Prueba de texto',
+            imagen: 3
+          },
+          { categoria: {
+            codigo : 1,
+            texto : 'Politica'
+          },
+          titulo : 'Vecinos fueron los primeros en enterarse',
+          texto: 'Prueba de texto',
+          imagen: 3
+        }
+  ];
   constructor() { }
 
   ngOnInit() {
